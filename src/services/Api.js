1a65e7f7a1e4e1781c6ff32d2300fe50286649c1
aharsha7 <aharsha7na@gmail.com>
2025-05-12 15:51:51 +0530

    final

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,9 +1,9 @@
-import axios from "axios";
+// import axios from "axios";
 
-const Api = axios.create({
-  // baseURL: "http://localhost:5000",
-  baseURL: "http://127.0.0.1:5000",
-});
+// const Api = axios.create({
+//   // baseURL: "http://localhost:5000",
+//   baseURL: "http://127.0.0.1:5000",
+// });
 
 export default Api;
 
